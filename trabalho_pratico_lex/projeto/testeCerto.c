dc0cdf8ba6df8097bc6f531606640a46ffd5fab0
BernardoPavani <bernardo.c.p.marinho@gmail.com>
2025-04-21 19:37:31 -0300

    feat: Adicionando regras da linguagem ao analisador lexico

---
+++ trabalho_pratico_lex/projeto/testeCerto.c
@@ -0,0 +1,19 @@
+int a;
+float b;
+char c;
+void funcao(int x, char y[]) {
+    int vetor[10];
+    c = 'z';
+    a = 123;
+    b = -4.2E+2;
+    print("Olá mundo");
+    if (a > 10) {
+        b = b + 1.0;
+    } else {
+        b = b - 1.0;
+    }
+    while (a != 0) {
+        a = a - 1;
+    }
+    return;
+}
---
+++ trabalho_pratico_lex/projeto/testeErro.c
@@ -0,0 +1,5 @@
+int 3nome;              /* erro: identificador começando com número*/
+float numero@;          /* erro: caractere inválido '@'*/
+char c = 'abc';         /* erro: char com mais de um caractere*/
+string s = "Olá;        /* erro: string não fechada*/
+print(“Mensagem”);      /* erro: aspas não padronizadas (aspas curvas)*/
